825c367d47b70beea790528fc40cfd4e07a6f294
Ross Waguespack <rosswaguespack@Rosss-MacBook-Pro.local>
2024-09-04 13:31:50 -0500

    a thing

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+createCanvas(200, 200);
+background(150);
+fill(500)
+ellipse(70, 130, 20, 50)
+ellipse(130, 130, 20, 50)
+fill(50)
+ellipse(75, 140, 10, 20)
+ellipse(125, 140, 10, 20)
+fill(500)
+ellipse(100, 100, 100, 100)
+fill(0)
+rect(90.5, 50, 20, 99.5)
+fill(500)
+ellipse(100, 90, 10, 10)
+ellipse(100, 110, 10, 10);
+}
+
+function draw() {
+
+}
